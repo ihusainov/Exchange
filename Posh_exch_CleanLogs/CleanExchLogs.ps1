1b2ff3318bf8ad8e80594db55c9bd79ee684058b
ih <62062799+ihusainov@users.noreply.github.com>
2021-08-24 22:56:22 +0300

    Create CleanExchLogs.ps1

---
+++ Posh_exch_CleanLogs/CleanExchLogs.ps1
@@ -0,0 +1,46 @@
+<#
+.SYNOPSIS 
+Delete exchange 2016 - 2019 logs from system directory
+ 
+.DESCRIPTION
+Exec from Powershell
+
+.NOTES 
+Written by:  ih
+ 
+Change Log  
+V0.2  14/09/2019 - Stable version
+#>
+
+
+Set-Executionpolicy RemoteSigned
+$days=0  # How many days keep log files
+$IISLogPath="C:\inetpub\logs\LogFiles\"
+$ExchangeLoggingPath="C:\Program Files\Microsoft\Exchange Server\V15\Logging\"
+$ETLLoggingPath="C:\Program Files\Microsoft\Exchange Server\V15\Bin\Search\Ceres\Diagnostics\ETLTraces\"
+$ETLLoggingPath2="C:\Program Files\Microsoft\Exchange Server\V15\Bin\Search\Ceres\Diagnostics\Logs"
+
+Function CleanLogfiles($TargetFolder)
+{
+  write-host -debug -ForegroundColor Yellow -BackgroundColor Cyan $TargetFolder
+
+    if (Test-Path $TargetFolder) {
+        $Now = Get-Date
+        $LastWrite = $Now.AddDays(-$days)
+    #   $Files = Get-ChildItem $TargetFolder -Include *.log,*.blg, *.etl -Recurse | Where {$_.LastWriteTime -le "$LastWrite"}
+        $Files = Get-ChildItem "C:\Program Files\Microsoft\Exchange Server\V15\Logging\"  -Recurse | Where-Object {$_.Name -like "*.log" -or $_.Name -like "*.blg" -or $_.Name -like "*.etl"}  | where {$_.lastWriteTime -le "$lastwrite"} | Select-Object FullName  
+        foreach ($File in $Files)
+            {
+               $FullFileName = $File.FullName  
+               Write-Host "Deleting file $FullFileName" -ForegroundColor "yellow"; 
+                Remove-Item $FullFileName -ErrorAction SilentlyContinue | out-null
+            }
+       }
+Else {
+    Write-Host "The folder $TargetFolder doesn't exist! Check the folder path!" -ForegroundColor "red"
+    }
+}
+CleanLogfiles($IISLogPath)
+CleanLogfiles($ExchangeLoggingPath)
+CleanLogfiles($ETLLoggingPath)
+CleanLogfiles($ETLLoggingPath2)
